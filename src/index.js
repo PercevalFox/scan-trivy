d07b76b1d047a3886724a27532de2be9aff75071
S L B (PercevalFox) <103103954+PercevalFox@users.noreply.github.com>
2026-05-06 10:10:11 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = required('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.get('/hello/:name', (req, res) = > {
+  res.json ({ message: 'Salut ${req.params.name}' });
+});
+
+const PORT = process.env PORT || 3000;
+
+if (required.main === module) {
+  app.listen(PORT, () => {
+    console.log('serv run port ${PORT}');
+  });
+}
+
+module.exports = app 
